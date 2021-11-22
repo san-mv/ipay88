95231ee7fda15b44852a1e717fd3d8edbf6fc119
Naif Mohamed <naifmhd@gmail.com>
2021-11-22 23:17:09 +0500

    updated the url

--- src/IPay88/Security/Response.php
+++ src/IPay88/Security/Response.php
@@ -4,8 +4,8 @@ namespace IPay88\Security;
 
 class Response
 {
-	private $merchantKey;
-	public static $validReferrer = "www.mobile88.com";
+    private $merchantKey;
+    public static $validReferrer = "www.ipay88.co.th";
 
     public function __construct($merchantKey)
     {
@@ -14,24 +14,22 @@ class Response
 
     public function validate($response)
     {
-        if($response->getReferrer() !== self::$validReferrer)
-        {
-        	throw new Exceptions\InvalidReferrerException;
+        if ($response->getReferrer() !== self::$validReferrer) {
+            throw new Exceptions\InvalidReferrerException;
         }
 
         $sig = Signature::generateSignature(
-        	$this->merchantKey,
-        	$response->getMerchantCode(),
-        	$response->getPaymentId(),
-        	$response->getRefNo(),
-        	preg_replace('/[\.\,]/', '', $response->getAmount()), //clear ',' and '.'
-        	$response->getCurrency(),
-        	$response->getStatus()
-        	);
+            $this->merchantKey,
+            $response->getMerchantCode(),
+            $response->getPaymentId(),
+            $response->getRefNo(),
+            preg_replace('/[\.\,]/', '', $response->getAmount()), //clear ',' and '.'
+            $response->getCurrency(),
+            $response->getStatus()
+        );
 
-        if($response->getSignature() !== $sig)
-        {
-        	throw new Exceptions\InvalidSignatureException;
+        if ($response->getSignature() !== $sig) {
+            throw new Exceptions\InvalidSignatureException;
         }
 
         return true;
